68405f71ae313f609f815f6ed19ad16fe84c9d82
whoisl <whoisl@163.com>
2022-09-07 17:21:29 +0800

    Update member.js

--- js/member.js
+++ js/member.js
@@ -1,14 +1,14 @@
 var member = [
-    { phone: '001', name: 'Shadow4998' },
-    { phone: '002', name: 'joker' },
-    { phone: '003:', name: 'Dd绿化带' },
-    { phone: '004', name: '如月枫' },
-    { phone: '005', name: '黑君ry' },
-    { phone: '006', name: '岚岚岚堇' },
-    { phone: '007', name: '今日份的XYZ' },
-    { phone: '008', name: '樂金旅社' },
-    { phone: '009', name: 'MintMonk' },
-    { phone: '010', name: 'choc' },
-    { phone: '011', name: '三困' },
-    { phone: '012', name: '十谦' }
+    { phone: '作者id:', name: 'Shadow4998' },
+    { phone: '作者id:', name: 'joker' },
+    { phone: '作者id:', name: 'Dd绿化带' },
+    { phone: '作者id:', name: '如月枫' },
+    { phone: '作者id:', name: '黑君ry' },
+    { phone: '作者id:', name: '岚岚岚堇' },
+    { phone: '作者id:', name: '今日份的XYZ' },
+    { phone: '作者id:', name: '樂金旅社' },
+    { phone: '作者id:', name: 'MintMonk' },
+    { phone: '作者id:', name: 'choc' },
+    { phone: '作者id:', name: '三困' },
+    { phone: '作者id:', name: '十谦' }
 ];
